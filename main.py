311c71ed53c335bc778cf59f097ec20631902651
Luis Mendez <dluismendezdj@gmail.com>
2023-01-06 16:41:08 -0400

    feat: create permutation methods

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from algorithms.factorial import Factorial
+# from algorithms.factorial import Factorial
+from algorithms.permutation import Permutation
 
 if __name__ == "__main__":
-    factorial: Factorial = Factorial(n=5)
-    print(factorial.iterate_factorial())
-    print(factorial.recur_factorial())
+    # region Factorial
+    # factorial: Factorial = Factorial(n=5)
+    # print(factorial.iterate_factorial())
+    # print(factorial.recur_factorial())
+    # endregion
+
+    # region Permutation
+    permutation: Permutation = Permutation(
+        combination="ABC", pocket="", iterate_str=list("123")
+    )
+    permutation.recur_permute()
+    permutation.iterate_permute()
+    # endregion
